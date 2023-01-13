a601b24ced40b8a6f6796de865483e63d1063203
dv <dvaug86@gmail.com>
2023-01-13 13:07:05 -0500

    fix rebull

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -3,7 +3,7 @@ import ActionButton from '@/shared/ActionButton';
 import { SelectedPage } from '@/shared/types';
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
-import SponsorRedbull from "@/assets/SponsorRedbull.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
@@ -92,7 +92,7 @@ const Home = ({ setSelectedPage }: Props) => {
                 <div className='h-[150px] w-full bg-purple-100 py-10'>
                     <div className='mx-auto w-5/6'>
                         <div className='flex w-3/5 items-center justify-between gap-8'>
-                            <img src={SponsorRedbull} alt="redbull-sponsor" />
+                            <img src={SponsorRedBull} alt="redbull-sponsor" />
                             <img src={SponsorForbes} alt="forbes-sponsor" />
                             <img src={SponsorFortune} alt="fortune-sponsor" />
                         </div>
